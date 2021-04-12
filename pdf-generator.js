fb312dafd12ed4e01782e5358147fd96434e5b1a
Benjamin Liu <yipingbenjamin.l@gmail.com>
2021-04-12 00:07:39 -0700

    reference right template and add fake data

--- pdf-generator.js
+++ pdf-generator.js
@@ -5,7 +5,7 @@ const handlebars = require("handlebars");
 
 async function createPDF(data){
 
-	var templateHtml = fs.readFileSync(path.join(process.cwd(), 'template-test.html'), 'utf8');
+	var templateHtml = fs.readFileSync(path.join(process.cwd(), 'template.html'), 'utf8');
 	var template = handlebars.compile(templateHtml);
 	var html = template(data);
 
@@ -40,32 +40,29 @@ async function createPDF(data){
 	await browser.close();
 }
 
-// const data = {
-// 	firstname: "Anthony Edward Stark",
-// 	address: "200 Park Ave, New York, NY 10166, USA",
-// }
-
-// for (var i = 0; i < arr.length; i++) {
-//     var data = {
-// 		firstname: arr[i][0],
-// 		address: arr[i][1],
-// 	}
-// 	console.log(data)
-// 	createPDF(data)
-// }
+var arr = [["Kyong Nevels","9396 Shipley St. Daphne, AL 36526"],
+		   ["Kamala Brackens","425 Oak Ave. Hanover Park, IL 60133"],
+		   ["Laurence Hasty","839 Fulton Ave. Burke, VA 22015"],
+		   ["Ardell Leedy","625 Durham Court Bristow, VA 20136"],
+		   ["Boyce Burlew","63 NW. Devonshire St. Randolph, MA 02368"],
+		   ["Diamond Winstead","877 Santa Clara Ave. Lumberton, NC 28358"],
+		   ["Cathleen Symes","8539 Kirkland Street Huntsville, AL 35803"],
+		   ["Mei Allman","7129 Lakewood Ave. Winter Springs, FL 32708"],
+		   ["Phylis Makin","8071 Roehampton Circle Buford, GA 30518"],
+		   ["Devona Porcaro","9508 Wrangler Lane Dawsonville, GA 30534"],
+		   ["Jackson Tilford","9168 Edgewater Avenue Butte, MT 59701"],
+		   ["Roland Dominy","101 Warren Lane Hamburg, NY 14075"],
+		   ["Ludie Levan","91 Elm Dr. Chaska, MN 55318"],
+		   ["Sherrell Adair","88 Canterbury Lane Oconomowoc, WI 53066"],
+		   ["Nicolasa Sowinski","8600 Winding Way Drive Gurnee, IL 60031"],
+		   ["Marget Zenon","350 West Summer Street Orland Park, IL 60462"],
+		   ["Chloe Shadwick","31 Constitution Ave. Sevierville, TN 37876"],
+		   ["Bailey Cady","28 Court Ave. Sewell, NJ 08080"],
+		   ["Farah Colby","62 South Shadow Brook Dr. Aiken, SC 29803"],
+		   ["Pasty Pavlik","750 Oakland Road Harlingen, TX 78552"]]
 
 
 var PromisePool = require('es6-promise-pool')
-
-var promiseProducer = function() {
-	for (var i = 0; i < arr.length; i++) {
-		var data = {
-
-		}
-		console.log(data)
-		createPDF(data)
-	}
-}
 
 var counter = 0
 var count = 0
